902ea6688e6c9f6c0bf25c1ba8520ac481d7edc5
matt-banschbach <mbanschbach@protonmail.ch>
2024-12-10 22:21:01 -0600

    Did a bunch of work on most of the basic implementations

---
+++ Supervised Learning/README.md
@@ -0,0 +1,19 @@
+
+
+
+### Directory Contents
+
+___
+### Datasets
+I chose to implement each algorithm on simple datasets loaded from Seaborn and Scikit-learn, 
+so that I could focus on learning the nuances of each algorithm. 
+
+
+
+I primarily use the penguins dataset, which can be loaded with the following
+
+```python
+import seaborn as sns
+sns.load_dataset('penguins')
+```
+
--- em_el/utils.py
+++ em_el/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 __all__=[
     'euclidean',
@@ -10,3 +12,24 @@ def euclidean(a, b):
 
 def entropy(class_probabilities: list) -> float:
     return sum([-p * np.log2(p) for p in class_probabilities if p > 0])
+
+
+def draw_confusion_matrix(cf_matrix, title):
+    plt.figure(figsize=(9, 6))
+    ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', cbar=False)
+
+    ax.set_title(title)
+    ax.set_xlabel('Predicted Values')
+    ax.set_ylabel('Actual Values')
+
+    num_rows, num_cols = cf_matrix.shape  # Get the number of rows and columns
+
+    # Draw horizontal lines
+    for i in range(num_rows + 1):
+        ax.axhline(y=i, color='black', linewidth=0.5)
+
+    # Draw vertical lines
+    for j in range(num_cols + 1):
+        ax.axvline(x=j, color='black', linewidth=0.5)
+
+    plt.show()
